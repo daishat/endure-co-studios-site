3ac9ea5b9a681e6b8fd37b5fa8142013fc0f6d22
daishat <daishasvoice@outlook.com>
2026-08-08 12:34:23 -0500

    Update comics-data.js

--- js/comics-data.js
+++ js/comics-data.js
@@ -7,10 +7,10 @@ Add each new comic here.
 
 Example:
 {
-  issue: "024",
+  issue: "023",
   title: "Your New Comic Title",
   date: "August 15, 2026",
-  image: "images/comics/issue-024.jpg"
+  image: "images/comics/issue-024.png"
 }
 */
 
@@ -18,133 +18,133 @@ window.COMICS = [
   {
     issue: "022",
     title: "Sibling Bond",
-    date: "",
-    image: "images/comics/issue-022.jpg"
+    date: "August 08, 2026",
+    image: "images/comics/issue-022.png"
   },
   {
     issue: "021",
     title: "Pillow Thief",
-    date: "",
-    image: "images/comics/issue-021.jpg"
+    date: "August 08, 2026",
+    image: "images/comics/issue-021.png"
   },
   {
     issue: "020",
     title: "Young & Rich vs Wise & Broke",
-    date: "",
-    image: "images/comics/issue-020.jpg"
+    date: "August 08, 2026",
+    image: "images/comics/issue-020.png"
   },
   {
     issue: "019",
     title: "I'm the New Adult",
-    date: "",
-    image: "images/comics/issue-019.jpg"
+    date: "August 06 , 2026",
+    image: "images/comics/issue-019.png"
   },
   {
     issue: "018",
     title: "My Family Special Timing",
-    date: "",
-    image: "images/comics/issue-018.jpg"
+    date: "August 01, 2026",
+    image: "images/comics/issue-018.png"
   },
   {
     issue: "017",
     title: "Life is Kicking My Butt",
-    date: "",
-    image: "images/comics/issue-017.jpg"
+    date: "July 28, 2026",
+    image: "images/comics/issue-017.png"
   },
   {
     issue: "016",
     title: "Commercials in Streaming Services",
-    date: "",
-    image: "images/comics/issue-016.jpg"
+    date: "July 10, 2026",
+    image: "images/comics/issue-016.png"
   },
   {
     issue: "015",
     title: "Grocery Shopping Chaos",
-    date: "",
-    image: "images/comics/issue-015.jpg"
+    date: "July 03, 2026",
+    image: "images/comics/issue-015.png"
   },
   {
     issue: "014",
     title: "Procrastination",
-    date: "",
-    image: "images/comics/issue-014.jpg"
+    date: "July 03, 2026",
+    image: "images/comics/issue-014.png"
   },
   {
     issue: "013",
     title: "North to South Matters",
-    date: "",
-    image: "images/comics/issue-013.jpg"
+    date: "July 03, 2026",
+    image: "images/comics/issue-013.png"
   },
   {
     issue: "012",
     title: "Introvert Sales Job",
-    date: "",
-    image: "images/comics/issue-012.jpg"
+    date: "July 01, 2026",
+    image: "images/comics/issue-012.png"
   },
   {
     issue: "011",
     title: "Okay by Myself",
-    date: "",
-    image: "images/comics/issue-011.jpg"
+    date: "July 01, 2026",
+    image: "images/comics/issue-011.png"
   },
   {
     issue: "010",
     title: "Life's Forecast",
-    date: "",
-    image: "images/comics/issue-010.jpg"
+    date: "July 01, 2026",
+    image: "images/comics/issue-010.png"
   },
   {
     issue: "009",
     title: "Love is Patience",
-    date: "",
-    image: "images/comics/issue-009.jpg"
+    date: "July 01, 2026",
+    image: "images/comics/issue-009.png"
   },
   {
     issue: "008",
     title: "Natural Hair Struggles",
-    date: "",
-    image: "images/comics/issue-008.jpg"
+    date: "July 01, 2026",
+    image: "images/comics/issue-008.png"
   },
   {
     issue: "007",
     title: "Stress of Staying Balance",
-    date: "",
-    image: "images/comics/issue-007.jpg"
+    date: "June 29, 2026",
+    image: "images/comics/issue-007.png"
   },
   {
     issue: "006",
     title: "Productivity vs Deadlines",
-    date: "",
-    image: "images/comics/issue-006.jpg"
+    date: "June 29, 2026",
+    image: "images/comics/issue-006.png"
   },
   {
     issue: "005",
     title: "My Calling",
-    date: "",
-    image: "images/comics/issue-005.jpg"
+    date: "June 28, 2026",
+    image: "images/comics/issue-005.png"
   },
   {
     issue: "004",
     title: "Life Goals",
-    date: "",
-    image: "images/comics/issue-004.jpg"
+    date: "June 28, 2026",
+    image: "images/comics/issue-004.png"
   },
   {
     issue: "003",
     title: "Imma Get This Money Somehow",
-    date: "",
-    image: "images/comics/issue-003.jpg"
+    date: "June 28, 2026",
+    image: "images/comics/issue-003.png"
   },
   {
     issue: "002",
     title: "Motherhood",
-    date: "",
-    image: "images/comics/issue-002.jpg"
+    date: "June 28, 2026",
+    image: "images/comics/issue-002.png"
   },
   {
     issue: "001",
     title: "Mental Health",
-    date: "",
-    image: "images/comics/issue-001.jpg"
+    date: "June 26, 2026",
+    image: "images/comics/issue-001.png"
   }
 ];
