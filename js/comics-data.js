a58263da693aecb80d40d0f5b8d260b64016aedd
daishat <daishasvoice@outlook.com>
2026-08-08 11:37:41 -0500

    Add all 22 comic issues

--- js/comics-data.js
+++ js/comics-data.js
@@ -16,21 +16,135 @@ Example:
 
 window.COMICS = [
   {
-    issue: "023",
-    title: "The Big Sister",
-    date: "August 08, 2026",
-    image: ""
-  },
-  {
     issue: "022",
-    title: "Pillow Thief",
-    date: "August 08, 2026",
-    image: ""
+    title: "Sibling Bond",
+    date: "",
+    image: "images/comics/issue-022.jpg"
   },
   {
     issue: "021",
+    title: "Pillow Thief",
+    date: "",
+    image: "images/comics/issue-021.jpg"
+  },
+  {
+    issue: "020",
     title: "Young & Rich vs Wise & Broke",
-    date: "August 08, 2026",
-    image: ""
+    date: "",
+    image: "images/comics/issue-020.jpg"
+  },
+  {
+    issue: "019",
+    title: "I'm the New Adult",
+    date: "",
+    image: "images/comics/issue-019.jpg"
+  },
+  {
+    issue: "018",
+    title: "My Family Special Timing",
+    date: "",
+    image: "images/comics/issue-018.jpg"
+  },
+  {
+    issue: "017",
+    title: "Life is Kicking My Butt",
+    date: "",
+    image: "images/comics/issue-017.jpg"
+  },
+  {
+    issue: "016",
+    title: "Commercials in Streaming Services",
+    date: "",
+    image: "images/comics/issue-016.jpg"
+  },
+  {
+    issue: "015",
+    title: "Grocery Shopping Chaos",
+    date: "",
+    image: "images/comics/issue-015.jpg"
+  },
+  {
+    issue: "014",
+    title: "Procrastination",
+    date: "",
+    image: "images/comics/issue-014.jpg"
+  },
+  {
+    issue: "013",
+    title: "North to South Matters",
+    date: "",
+    image: "images/comics/issue-013.jpg"
+  },
+  {
+    issue: "012",
+    title: "Introvert Sales Job",
+    date: "",
+    image: "images/comics/issue-012.jpg"
+  },
+  {
+    issue: "011",
+    title: "Okay by Myself",
+    date: "",
+    image: "images/comics/issue-011.jpg"
+  },
+  {
+    issue: "010",
+    title: "Life's Forecast",
+    date: "",
+    image: "images/comics/issue-010.jpg"
+  },
+  {
+    issue: "009",
+    title: "Love is Patience",
+    date: "",
+    image: "images/comics/issue-009.jpg"
+  },
+  {
+    issue: "008",
+    title: "Natural Hair Struggles",
+    date: "",
+    image: "images/comics/issue-008.jpg"
+  },
+  {
+    issue: "007",
+    title: "Stress of Staying Balance",
+    date: "",
+    image: "images/comics/issue-007.jpg"
+  },
+  {
+    issue: "006",
+    title: "Productivity vs Deadlines",
+    date: "",
+    image: "images/comics/issue-006.jpg"
+  },
+  {
+    issue: "005",
+    title: "My Calling",
+    date: "",
+    image: "images/comics/issue-005.jpg"
+  },
+  {
+    issue: "004",
+    title: "Life Goals",
+    date: "",
+    image: "images/comics/issue-004.jpg"
+  },
+  {
+    issue: "003",
+    title: "Imma Get This Money Somehow",
+    date: "",
+    image: "images/comics/issue-003.jpg"
+  },
+  {
+    issue: "002",
+    title: "Motherhood",
+    date: "",
+    image: "images/comics/issue-002.jpg"
+  },
+  {
+    issue: "001",
+    title: "Mental Health",
+    date: "",
+    image: "images/comics/issue-001.jpg"
   }
 ];
