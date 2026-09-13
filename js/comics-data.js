1369cbdf4483f81b9e3cfd7cd2e6c6a337e48058
daishat <daishasvoice@outlook.com>
2026-09-13 09:22:58 -0500

    Update comics-data.js
    Fix comic image paths for issues 028-033

--- js/comics-data.js
+++ js/comics-data.js
@@ -18,37 +18,37 @@ Example:
     issue: "033",
     title: "Not the Nice Me Anymore",
     date: "September 11, 2026",
-    image: "images/comics/issue-033.png"
+    image: "images/comics/issue- 033.png"
   },
   {
     issue: "032",
     title: "Same Brain, Just Older",
     date: "September 11, 2026",
-    image: "images/comics/issue-032.png"
+    image: "images/comics/issue- 032.png"
   },
   {
     issue: "031",
     title: "Mom After Hours",
     date: "September 10, 2026",
-    image: "images/comics/issue-031.png"
+    image: "images/comics/issue- 031.png"
   },
   {
     issue: "030",
     title: "That’s Your Job, Mom",
     date: "September 4, 2026",
-    image: "images/comics/issue-030.png"
+    image: "images/comics/issue- 030.png"
   },
   {
     issue: "029",
     title: "Mom’s 2 A.M. Peace",
     date: "September 3, 2026",
-    image: "images/comics/issue-029.png"
+    image: "images/comics/issue- 029.png"
   },
   {
     issue: "028",
     title: "The Mail Truck",
     date: "September 3, 2026",
-    image: "images/comics/issue-028.png"
+    image: "images/comics/issue- 028.png"
   },
 
     {
